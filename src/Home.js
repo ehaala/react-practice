d6ea0a1a390e5f028f2bc346eec8123d334d700c
ehaala <ejhaala@gmail.com>
2017-12-06 02:08:04 -0800

    added material ui and flexbox grid

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import { RaisedButton, Tabs, Tab } from 'material-ui';
+import { Row, Col } from 'react-flexbox-grid';
+import Slider from 'material-ui/Slider';
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+};
 
 class Home extends Component {
 	render() {
 		return (
-			<h1>Home Page</h1>
+			<div>
+				<h1>Welcome</h1>
+
+				<Row>
+				<Col sm={1} />
+				<Col sm={10}>
+			  <Tabs>
+			    <Tab label="Item One" >
+			      <div>
+			        <h2 style={styles.headline}>Tab One</h2>
+			        <p>
+			          This is an example tab.
+			        </p>
+			      </div>
+			    </Tab>
+			    <Tab label="Item Two" >
+			      <div>
+			        <h2 style={styles.headline}>Tab Two</h2>
+			        <p>
+			          This is another example tab.
+			        </p>
+			      </div>
+			    </Tab>
+			  </Tabs>
+			  </Col>
+			  <Col sm={1} />
+			  </Row>
+
+			</div>
 		);
 	}
 }
---
+++ src/muiTheme.js
@@ -0,0 +1,29 @@
+import {
+  cyanA400, cyan700,
+  yellowA200, deepPurpleA700,
+  grey100, grey300, grey400, grey500,
+  white, darkBlack, fullWhite,
+} from 'material-ui/styles/colors';
+import {fade} from 'material-ui/utils/colorManipulator';
+import spacing from 'material-ui/styles/spacing';
+
+export default {
+  spacing: spacing,
+  fontFamily: 'Roboto, sans-serif',
+  palette: {
+    primary1Color: deepPurpleA700,
+    primary2Color: cyan700,
+    primary3Color: grey400,
+    accent1Color: yellowA200,
+    accent2Color: grey100,
+    accent3Color: grey500,
+    textColor: fullWhite,
+    secondaryTextColor: fade(fullWhite, 0.7),
+    alternateTextColor: fullWhite,
+    canvasColor: '#303030',
+    borderColor: fade(fullWhite, 0.3),
+    disabledColor: fade(fullWhite, 0.3),
+    pickerHeaderColor: fade(fullWhite, 0.12),
+    clockCircleColor: fade(fullWhite, 0.12),
+  },
+};
